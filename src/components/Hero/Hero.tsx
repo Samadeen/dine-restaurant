a7a25d20f75f9573bd10100e56be5bb71dee2885
Momoh Abdulsamad <samadmomoh@gmail.com>
2024-01-31 09:07:43 +0100

    Update Hero.tsx

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,25 @@
+import styles from './Hero.module.scss';
+import dine from '../../assets/dine.svg';
+import Button from '../Button/Button';
+
+const Hero = () => {
+  return (
+    <section>
+      <div className={styles.hero_container}>
+        <div className={styles.hero_text}>
+          <img src={dine} alt='dine logo' />
+          <h1>
+            Exquisite dining <br /> since 1989
+          </h1>
+          <p>
+            Experience our seasonal menu in beautiful country surroundings. Eat
+            the freshest produce from the comfort of our farmhouse.
+          </p>
+          <Button text='Book a table' transparent={true} />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
