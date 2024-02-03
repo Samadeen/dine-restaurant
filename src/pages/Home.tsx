9b7a5b5862385409268f04b5234baa8f1fec3018
Momoh Abdulsamad <samadmomoh@gmail.com>
2024-02-03 04:48:59 +0100

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,8 @@
+import CTO from '../components/CTO/CTO';
+import Events from '../components/Events/Events';
+import Footer from '../components/Footer/Footer';
 import Hero from '../components/Hero/Hero';
+import Highlight from '../components/Highlight/Highlight';
 import Showcase from '../components/Showcase/Showcase';
 
 const Home = () => {
@@ -6,6 +10,10 @@ const Home = () => {
     <div>
       <Hero />
       <Showcase />
+      <Highlight />
+      <Events />
+      <CTO />
+      <Footer />
     </div>
   );
 };
